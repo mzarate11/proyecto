8466ff33f405bed7b272f2104ed0058ab195061f
carlosgn9090 <carlosgn90@hotmailcom>
2018-03-29 17:25:27 -0600

    actualizando

---
+++ Proyecto_2_Progra_3/Proyecto_2_PL/Proyecto_2_VentanasDelMenu/NuevoModificar/frm_Mostrar_Estados.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Proyecto_2_DAL.Catalogos_y_Mantenimientos;
+
+namespace Proyecto_2_PL.Proyecto_2_VentanasDelMenu.NuevoModificar
+{
+    public partial class frm_Mostrar_Estados : Form
+    {
+        public cls_Estados_DAL Obj_DAL_Estados;
+        public frm_Mostrar_Estados()
+        {
+            InitializeComponent();
+        }
+
+        private void frm_Mostrar_Estados_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
