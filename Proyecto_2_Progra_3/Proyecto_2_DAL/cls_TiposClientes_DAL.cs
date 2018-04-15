945b9c65d6b1d6378418ed3056d44c26b5f0f8d3
willyesmo <37908991+willyesmo@users.noreply.github.com>
2018-04-15 10:38:49 -0600

    rafa y jeff se la comen

---
+++ Proyecto_2_Progra_3/Proyecto_2_DAL/cls_TiposClientes_DAL.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_2_DAL.Catalogos_y_Mantenimientos
+{
+   public class cls_TiposClientes_DAL
+    {
+
+        private char _cIdEstado,
+                     _cBandAX;
+        private string _sDescripcion, _sTipoCliente;
+
+        private int _iIdTipoCliente;
+
+        #region
+        public char CIdEstado
+        {
+            get
+            {
+                return _cIdEstado;
+            }
+
+            set
+            {
+                _cIdEstado = value;
+            }
+        }
+
+        public char CBandAX
+        {
+            get
+            {
+                return _cBandAX;
+            }
+
+            set
+            {
+                _cBandAX = value;
+            }
+        }
+
+        public string SDescripcion
+        {
+            get
+            {
+                return _sDescripcion;
+            }
+
+            set
+            {
+                _sDescripcion = value;
+            }
+        }
+
+        public string STipoCliente
+        {
+            get
+            {
+                return _sTipoCliente;
+            }
+
+            set
+            {
+                _sTipoCliente = value;
+            }
+        }
+
+        public int IIdTipoCliente
+        {
+            get
+            {
+                return _iIdTipoCliente;
+            }
+
+            set
+            {
+                _iIdTipoCliente = value;
+            }
+        }
+        #endregion
+    }
+}
